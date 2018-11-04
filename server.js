f4270a56b205804062a325f9fe520342ca042017
Javier Solares Servent <javiss1254@hotmail.com>
2018-11-03 23:26:50 -0600

    Se agregó una "conexión" al archivo de rutas en el archivo "server.js".

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var bodyparser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
+const router = require('./rutas/api');
 
 mongoose.connect('mongodb://localhost/pokedex');
+
+app.use(cors());
+
+app.use(bodyparser.json());
+
+app.use('/api/v1/pokemon', router);
 
 app.listen(5000, function() {
     console.log('Node server is running..');
